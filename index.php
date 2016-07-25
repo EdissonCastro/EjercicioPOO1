6653375f313b13679edbf8379d08f126f61eaed8
EdissonCastro <eacastro187@misena.edu.co>
2016-07-25 11:40:42 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="estilos.css" rel="stylesheet" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Ejercicio POO</title>
+
+</head>
+<body>
+<form id="form1" name="form1" method="post" action="validar.php">
+  <div align="center">
+    <table class="formulario" width="637" border="1">
+      <tr>
+        <td height="167"><img src="banner.jpg" width="627" height="163" /></td>
+      </tr>
+      <tr>
+        <td height="301"><div align="center">
+          <table class="formulario" width="484" border="1">
+            <tr>
+              <td width="238"><div align="right">Contraseña</div></td>
+              <td width="230">
+                <input type="text" name="clave" id="textfield" /></td>
+              </tr>
+            <tr>
+              <td><div align="right">¿Cuanto es 2 x 8?</div></td>
+              <td><input type="text" name="respuesta" id="textfield2" /></td>
+              </tr>
+            <tr>
+              <td colspan="2">
+                <div align="center">
+                  <input type="submit" name="button" id="button" value="Enviar" />
+                  </div>
+                </td>
+              </tr>
+              
+              <td colspan="2">
+                
+                </td>
+              </tr>
+              
+            </table>
+        </div></td>
+      </tr>
+      <tr>
+        <td height="178"><div align="center"><img src="footer.jpg" width="386" height="214" /></div></td>
+      </tr>
+  </table>
+  </div>
+</form>
+</body>
+</html>
